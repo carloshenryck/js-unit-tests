a11c90f286f201f16bbd9172735ec9a274c0931a
Henryck <ryck302@gmail.com>
2022-05-13 01:33:15 -0300

    requisito 1 ao 3

--- src/average.js
+++ src/average.js
@@ -11,8 +11,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // Adicione seu código aqui
+const average = (arr) => {
+  if (Array.isArray(arr) === false || arr.length === 0) { return undefined; }
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== 'number') { return undefined; }
+    sum += arr[i];
+  }
+
+  return Math.round(sum / arr.length);
 };
 
 module.exports = average;
